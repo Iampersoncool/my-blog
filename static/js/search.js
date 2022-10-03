5ee9b217509f2f83312e84936c2f70cbbe435385
Iampersoncool <86845476+Iampersoncool@users.noreply.github.com>
2022-10-03 16:29:00 -0700

    changed search

--- static/js/search.js
+++ static/js/search.js
@@ -1,32 +1,34 @@
 const searchInput = document.querySelector('.searchInput')
 const blogs = document.querySelectorAll('.blog')
 
+let searchResults
+
 async function getDocuments() {
   try {
     const response = await fetch('/index.json')
     const data = await response.json()
 
-    return data
+    searchResults = data.map(d => {
+      return { Description: d.Description, Title: d.Title}
+    })
   } catch (e) {
     console.log(e)
   }
 }
 
-async function search(value) {
-  const documents = await getDocuments()
+getDocuments()
 
-  documents.forEach((document, i) => {
+async function search(value) {
+  searchResults?.forEach((result, i) => {
     const isVisible =
-      document.Title.toLowerCase().includes(value.trim()) ||
-      document.Description.toLowerCase().includes(value.trim())
+      result.Title.toLowerCase().includes(value.trim()) ||
+      result.Description.toLowerCase().includes(value.trim())
 
     blogs[i].classList.toggle('hidden', !isVisible)
   })
-
 }
 
 searchInput.addEventListener('input', (e) => {
   const value = e.target.value.toLowerCase()
-
   search(value)
 })
